66e16b32ff120ebb23338d714d2096c7661ec6a3
Gloria Paulina Gonzalez Hernandez <ghpaulina3@gmail.com>
2022-09-10 15:56:57 -0600

    commit tree d5c05c2499b2ee47c728af86721545bc23566c1f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function logCats() {
+    ["Milo", "Otis", "Garfield"];
+}
+console.log(cats);
+
+const destructivelyAppendCat = ["Milo", "Otis", "Garfield"];
+destructivelyAppendCat.push("Ralph")
+console.log(destructivelyAppendCat);
+
+
+
+
+
+
+
+
+
